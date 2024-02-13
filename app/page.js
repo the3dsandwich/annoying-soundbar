370901e3da47f1037a177db50a90c88e56c7af76
Shann Wei Yeh <weiwei.sw.yeh@gmail.com>
2024-02-13 23:26:03 +0800

    [dev] rewrite in Next

---
+++ app/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import "semantic-ui-css/semantic.min.css";
+import Nav from "../components/Nav";
+import Main from "../components/Main";
+
+export default function Home() {
+  return (
+    <div>
+      <Nav />
+      <Main />
+    </div>
+  );
+}
